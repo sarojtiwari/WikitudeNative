241fca17dd08c76be0e657f3fc423608d9400e88
Saroj Tiwari <sarojtiwari704@gmail.com>
2022-05-04 09:12:52 +0545

    Initial Commit

---
+++ WikitudeNative/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WikitudeNative
+//
+//  Created by Saroj Tiwari on 04/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
